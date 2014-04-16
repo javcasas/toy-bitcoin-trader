54bb3c2062034f3e8c86400afe9028185f3c3bfc
Javier Casas <javcasas@gmail.com>
2014-04-16 21:35:02 +0100

    Storing wallet

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 import json
 import time
 from datetime import datetime
+import pickle
+import os
 
 class MovingAverage(object):
     def __init__(self, npoints):
@@ -17,41 +19,48 @@ class MovingAverage(object):
 
 
 def main():
+    wallet = {}
     _20min_ave = MovingAverage(20)
     _200min_ave = MovingAverage(200)
-    btc = 1.0
-    usd = 0.0
+    if os.path.isfile("wallet.pickle"):
+        wallet = pickle.load(file("wallet.pickle"))
+    else:
+        wallet['btc'] = 1.0
+        wallet['usd'] = 0.0
     try:
         while True:
-            r = requests.get('https://blockchain.info/ticker').text
-            data = json.loads(r)
-            val = float(data['USD']['last'])
+            try:
+                r = requests.get('https://blockchain.info/ticker').text
+                data = json.loads(r)
+                val = float(data['USD']['last'])
+            except Exception:
+                time.sleep(0.5)
+                continue
             _20min_ave.add_point(val)
             _200min_ave.add_point(val)
             print
             print
             print "%s" % datetime.now().isoformat()
             print "20: {}, 200:{}".format(_20min_ave.average(), _200min_ave.average())
-            print "USD: {}, BTC:{}".format(usd, btc)
+            print "USD: {}, BTC:{}".format(wallet['usd'], wallet['btc'])
             if _20min_ave.average() > _200min_ave.average():
                 # Up trend, buy BTC
-                if usd > 0:
+                if wallet['usd'] > 0:
                     print "Buying BTC"
-                    btc = usd / val
-                    usd = 0.0
+                    wallet['btc'] = wallet['usd'] / val
+                    wallet['usd'] = 0.0
             elif _20min_ave.average() < _200min_ave.average():
                 # down trend, sell BTC
-                if btc > 0:
+                if wallet['btc'] > 0:
                     print "Selling BTC"
-                    usd = btc * val
-                    btc = 0.0
+                    wallet['usd'] = wallet['btc'] * val
+                    wallet['btc'] = 0.0
 
             time.sleep(60)
     finally:
-        print "Initial status:"
-        print "USD: {}, BTC:{}".format(0.0, 1.0)
         print "Final status:"
-        print "USD: {}, BTC:{}".format(usd, btc)
+        print "USD: {}, BTC:{}".format(wallet['usd'], wallet['btc'])
+        pickle.dump(wallet, file("wallet.pickle", "w"))
 
 if __name__ == "__main__":
     main()
