81f64ca10b68e896215904a8671153cc9e44175f
Javier Casas <javcasas@gmail.com>
2015-04-24 11:59:50 -0500

    Refactor

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from datetime import datetime
 import pickle
 import os
 
+
 class MovingAverage(object):
+    """
+    Implements a Moving Average, of the specified points
+    """
     def __init__(self, npoints):
         self.npoints = npoints
         self.points = []
@@ -18,35 +22,60 @@ class MovingAverage(object):
         return sum(self.points) / len(self.points)
 
 
-def main():
-    logfile = open("data.txt", "a")
-    wallet = {}
+def get_btc_price():
+    """
+    Connects to blockchain.info, and returns the last BTC/USD quote
+    """
+    while True:
+        try:
+            r = requests.get('https://blockchain.info/ticker').text
+            data = json.loads(r)
+            val = float(data['USD']['last'])
+            return val
+        except Exception, e:
+            print e
+            time.sleep(0.5)
+
+
+def wallet_status(wallet):
+    """
+    Returns a readable string for the specified wallet
+    """
+    return "Wallet: USD:{} BTC:{}".format(wallet['usd'], wallet['btc'])
+
+
+def main(wallet_fn="wallet.pickle"):
+    """
+    Main function. Reads wallet and simulates trading
+    """
+    tick_time = 3  # 1 check every minute
+
     _20min_ave = MovingAverage(20)
     _200min_ave = MovingAverage(200)
-    if os.path.isfile("wallet.pickle"):
-        wallet = pickle.load(file("wallet.pickle"))
+
+    # Load/recreate wallet
+    if os.path.isfile(wallet_fn):
+        wallet = pickle.load(file(wallet_fn))
     else:
+        # Start with 1BTC
+        wallet = {}
         wallet['btc'] = 1.0
         wallet['usd'] = 0.0
+
     try:
+        # Do forever, until Ctrl-C
         while True:
-            try:
-                r = requests.get('https://blockchain.info/ticker').text
-                data = json.loads(r)
-                val = float(data['USD']['last'])
-                logfile.write("%s\n" % datetime.now().isoformat())
-                logfile.write("%s\n\n" % r.encode("ascii", "ignore"))
-            except Exception, e:
-                print e
-                time.sleep(0.5)
-                continue
+            val = get_btc_price()
             _20min_ave.add_point(val)
             _200min_ave.add_point(val)
+
+            # Some logging
             print
-            print
-            print "%s" % datetime.now().isoformat()
-            print "20: {}, 200:{}".format(_20min_ave.average(), _200min_ave.average())
-            print "USD: {}, BTC:{}".format(wallet['usd'], wallet['btc'])
+            print "Timestamp:", datetime.now().isoformat()
+            print "Averages:", "20:", _20min_ave.average(), "200:", _200min_ave.average()
+            print wallet_status(wallet)
+
+            # Simulate operation
             if _20min_ave.average() > _200min_ave.average():
                 # Up trend, buy BTC
                 if wallet['usd'] > 0:
@@ -60,12 +89,11 @@ def main():
                     wallet['usd'] = wallet['btc'] * val
                     wallet['btc'] = 0.0
 
-            time.sleep(60)
+            time.sleep(tick_time)
     finally:
         print "Final status:"
-        print "USD: {}, BTC:{}".format(wallet['usd'], wallet['btc'])
+        print wallet_status(wallet)
         pickle.dump(wallet, file("wallet.pickle", "w"))
-        logfile.close()
 
 if __name__ == "__main__":
     main()
